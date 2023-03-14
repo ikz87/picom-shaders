db0a9c002ce5f56004c07f0954405a38193cab04
ikz87 <iikz87ii@gmail.com>
2023-03-14 14:38:07 -0600

    Add camera template shader

---
+++ 3DCamera/camera.glsl
@@ -0,0 +1,102 @@
+
+#version 330
+#define PI 3.14159265
+
+// These shaders work by using a pinhole camera and raycasting
+// The window 3d objects will always be (somewhat) centered at (0, 0, 0)
+struct pinhole_camera
+{
+    float focal_offset; // Distance along the Z axis between the camera 
+                        // center and the focal point. Use negative values
+                        // so the image doesn't flip
+                        // This kinda works like FOV in games
+
+    // Transformations 
+    // Use these to modify the coordinate system of the camera plane
+    vec3 rotations; // Rotations in radians around each axis 
+                    // The camera plane rotates around 
+                    // its center point, not the origin
+
+    vec3 translations; // Translations in pixels along each axis
+
+    vec3 deformations; // Deforms the camera. Higher values on each axis
+                       // means the window will be squashed in that axis
+
+    // ---------------------------------------------------------------// 
+    
+    // "Aftervalues" 
+    // These will be set later with setup_camera(), leave them as 0
+    vec3 base_x;
+    vec3 base_y;
+    vec3 base_z;
+    vec3 center_point;
+    vec3 focal_point;
+};
+
+// Sets up a camera by applying transformations and 
+// calculating xyz vector basis 
+pinhole_camera setup_camera(pinhole_camera camera)
+{
+    // Apply translations
+    camera.center_point += camera.translations;
+
+    // Apply rotations 
+    // We initialize our vector basis as normalized vectors
+    // in each axis * our deformations vector
+    camera.base_x = vec3(camera.deformations.x, 0, 0);
+    camera.base_y = vec3(0, camera.deformations.y, 0);
+    camera.base_z = vec3(0, 0, camera.deformations.z);
+
+
+    // Then we rotate them around following our rotations vector:
+    // First save these values to avoid redundancy
+    float cosx = cos(camera.rotations.x);
+    float cosy = cos(camera.rotations.y);
+    float cosz = cos(camera.rotations.z);
+    float sinx = sin(camera.rotations.x);
+    float siny = sin(camera.rotations.y);
+    float sinz = sin(camera.rotations.z);
+    
+    // Declare a buffer vector we will use to apply multiple changes at once
+    vec3 tmp = vec3(0);
+
+    // Rotations for base_x:
+    tmp = camera.base_x;
+    // Y axis:
+    tmp.x =  camera.base_x.x * cosy + camera.base_x.z * siny;
+    tmp.z = -camera.base_x.x * siny + camera.base_x.z * cosy;
+    camera.base_x = tmp;
+    // Z axis:
+    tmp.x =  camera.base_x.x * cosz - camera.base_x.y * sinz;
+    tmp.y =  camera.base_x.x * sinz + camera.base_x.y * cosz;
+    camera.base_x = tmp;
+
+    // Rotations for base_y:
+    tmp = camera.base_y;
+    // X axis:
+    tmp.y =  camera.base_y.y * cosx - camera.base_y.z * sinx;
+    tmp.z =  camera.base_y.y * sinx + camera.base_y.z * cosx;
+    camera.base_y = tmp;
+    // Z axis:
+    tmp.x =  camera.base_y.x * cosz - camera.base_y.y * sinz;
+    tmp.y =  camera.base_y.x * sinz + camera.base_y.y * cosz;
+    camera.base_y = tmp;
+
+    // Rotations for base_z: 
+    tmp = camera.base_z;
+    // Y axis:
+    tmp.x =  camera.base_z.x * cosy + camera.base_z.z * siny;
+    tmp.z = -camera.base_z.x * siny + camera.base_z.z * cosy;
+    camera.base_z = tmp;
+    // X axis:
+    tmp.y =  camera.base_z.y * cosx - camera.base_z.z * sinx;
+    tmp.z =  camera.base_z.y * sinx + camera.base_z.z * cosx;
+    camera.base_z = tmp;
+
+    // Now that we have our transformed 3d orthonormal base 
+    // we can calculate our focal point 
+    camera.focal_point = camera.center_point + camera.base_z * camera.focal_offset;
+
+    // Return our set up camera
+    return camera;
+}
