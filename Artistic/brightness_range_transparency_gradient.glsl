d872dd220c6413fe432970f90a34a009d76c8c67
Quantenzitrone <quantenzitrone@protonmail.com>
2023-04-26 06:21:26 +0200

    formatted to snake_case, variables as uniform at the top

--- Artistic/brightness_range_transparency_gradient.glsl
+++ Artistic/brightness_range_transparency_gradient.glsl
@@ -1,7 +1,18 @@
 #version 330
 in vec2 texcoord;             // texture coordinate of the fragment
-
 uniform sampler2D tex;        // texture of the window
+
+// tweak the following variablee and functions for your needs:
+// brightness value for the maximum transparency
+uniform float median_brightness = 1;
+// maximum brightness derivation from the median_brightness
+uniform float max_derivation = 0.2;
+// e.g. 0.5±0.25 -> gradient from #444 to #888 to #bbb with #888 being the least opaque
+// 0±0.25 -> gradient from #000 to #444 with #000 being at min_opacity
+// opacity for the median_brightness
+uniform float min_opacity = 0.9;
+// exponent for the gradient (e.g. 1 for linear, 2 for quadratic, etc)
+uniform int power = 2;
 
 // Default window post-processing:
 // 1) invert color
@@ -16,27 +27,14 @@ vec4 default_post_processing(vec4 c);
 vec4 window_shader() {
     vec4 c = texelFetch(tex, ivec2(texcoord), 0);
 
-    /*### define tweakable variables ###*/
-    // brightness value for the maximum transparency
-    float medianBrightness = 1;
-    // maximum brightness derivation from the medianBrightness
-    float maxDerivation = 0.2;
-    // e.g. 0.5±0.25 -> gradient from #444 to #888 to #bbb with #888 being the least opaque
-    // 0±0.25 -> gradient from #000 to #444 with #000 being at minOpacity
-    // opacity for the medianBrightness
-    float minOpacity = 0.9;
-    // exponent for the gradient (e.g. 1 for linear, 2 for quadratic, etc)
-    int power = 2;
-
-    /*### do the thing ###*/
-    // get the brightness derivation from the medianBrightness and normalize it
-    float normalizedDerivation = abs((c.r+c.g+c.b)/3 - medianBrightness)/maxDerivation;
+    // get the brightness derivation from the median_brightness and normalize it
+    float normalized_derivation = abs((c.r+c.g+c.b)/3 - median_brightness)/max_derivation;
     // only add transparency, if the pixel is not already transparent
-    if (c.a == 1 && normalizedDerivation < 1) {
+    if (c.a == 1 && normalized_derivation < 1) {
         // apply the gradient curvature
-        normalizedDerivation = (1-pow(1-normalizedDerivation, power));
+        normalized_derivation = 1-pow(1-normalized_derivation, power);
         // apply transparency to rgb and alpha, because glx uses premultiplied alpha
-        c *= minOpacity + normalizedDerivation * (1-minOpacity);
+        c *= min_opacity + normalized_derivation * (1-min_opacity);
     }
 
     return default_post_processing( c );
--- Artistic/color_range_transparency_gradient.glsl
+++ Artistic/color_range_transparency_gradient.glsl
@@ -1,7 +1,25 @@
 #version 330
 in vec2 texcoord;             // texture coordinate of the fragment
-
 uniform sampler2D tex;        // texture of the window
+
+// tweak the following variablee and functions for your needs:
+// rgb value for the maximum transparency
+uniform vec3 median_color = vec3(1);
+// maximum derivation from the median_color of each color channel (rgb)
+uniform vec3 max_derivation = vec3(0.2);
+// e.g. (1,0,0)±(0.2,0.2,0.2) -> gradient from #c00 to #f00 to #f33 with #f00 being the least opaque
+// (0,0,0)±(0.25,0.25,0,25) -> gradient from #000 to #444 with #000 being at min_opacity
+// opacity for the median_color
+uniform float min_opacity = 0.9;
+// exponent for the gradient (e.g. 1 for linear, 2 for quadratic, etc)
+uniform int power = 2;
+// use mean for a different effect
+float get_float (vec3 c) {
+    // maximum
+    return max(max(c.r,c.g),c.b);
+    // mean
+    // return (v.r + v.g + v.b)/3;
+}
 
 // Default window post-processing:
 // 1) invert color
@@ -10,40 +28,21 @@ uniform sampler2D tex;        // texture of the window
 // 4) rounded corners
 vec4 default_post_processing(vec4 c);
 
-// use mean for a different effect
-float getFloat (vec3 c) {
-    // maximum
-    return max(max(c.r,c.g),c.b);
-    // mean
-    // return (v.r + v.g + v.b)/3;
-}
+
 // Default window shader:
 // 1) fetch the specified pixel
 // 2) apply default post-processing
 vec4 window_shader() {
     vec4 c = texelFetch(tex, ivec2(texcoord), 0);
 
-    /*### define tweakable variables ###*/
-    // rgb value for the maximum transparency
-    vec3 medianColor = vec3(1);
-    // maximum derivation from the medianColor of each color channel (rgb)
-    vec3 maxDerivation = vec3(0.2);
-    // e.g. (1,0,0)±(0.2,0.2,0.2) -> gradient from #c00 to #f00 to #f33 with #f00 being the least opaque
-    // (0,0,0)±(0.25,0.25,0,25) -> gradient from #000 to #444 with #000 being at minOpacity
-    // opacity for the medianColor
-    float minOpacity = 0.9;
-    // exponent for the gradient (e.g. 1 for linear, 2 for quadratic, etc)
-    int power = 2;
-
-    /*### do the thing ###*/
-    // get the each colorchannels derivation from the medianColor channels and normalize them
-    vec3 normalizedDerivation = abs(c.rgb - medianColor)/maxDerivation;
+    // get the each colorchannels derivation from the median_color channels and normalize them
+    vec3 normalized_derivation = abs(c.rgb - median_color)/max_derivation;
     // only add transparency, if the pixel is not already transparent
-    if (c.a == 1 && getFloat(normalizedDerivation) < 1) {
+    if (c.a == 1 && get_float(normalized_derivation) < 1) {
         // apply the gradient curvature
-        normalizedDerivation = vec3(1)-pow(vec3(1)-normalizedDerivation, vec3(power));
+        normalized_derivation = vec3(1)-pow(vec3(1)-normalized_derivation, vec3(power));
         // apply transparency to rgb and alpha, because glx uses premultiplied alpha
-        c *= minOpacity + getFloat(normalizedDerivation) * (1-minOpacity);
+        c *= min_opacity + get_float(normalized_derivation) * (1-min_opacity);
     }
 
     return default_post_processing( c );
