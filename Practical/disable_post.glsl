96ce821ae40d6a7e0ba589355b7308e7d5b1a683
ikz87 <iikz87ii@gmail.com>
2024-08-10 00:36:06 -0600

    Wow a new commit after a while

---
+++ Practical/disable_post.glsl
@@ -0,0 +1,21 @@
+#version 330
+in vec2 texcoord;             // texture coordinate of the fragment
+
+uniform sampler2D tex;        // texture of the window
+
+// Default window post-processing:
+// 1) invert color
+// 2) opacity / transparency
+// 3) max-brightness clamping
+// 4) rounded corners
+vec4 default_post_processing(vec4 c);
+
+// Default window shader:
+// 1) fetch the specified pixel
+// 2) apply default post-processing
+vec4 window_shader() {
+    vec4 c = texelFetch(tex, ivec2(texcoord), 0);
+    // return default_post_processing(c); <-- Not sure, but maybe not calling this 
+    //                                        on certain windows improves performance a little lol
+    return c;
+}
